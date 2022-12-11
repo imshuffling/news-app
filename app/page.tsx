8ed2101a385ef53dd9d2acba47c9f6934e71dbab
David Riches <david.riches@appnovation.com>
2022-12-11 21:19:01 +0000

    adding first commit

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+import { categories } from "../constants";
+import fetchNews from "../lib/fetchNews";
+import NewsList from "./NewsList";
+import MOCK_DATA from "../response.json";
+
+async function Homepage() {
+  const news: NewsResponse =
+    MOCK_DATA || (await fetchNews(categories.join(",")));
+
+  //   const news: NewsResponse = await fetchNews(categories.join(","));
+
+  console.log(news);
+
+  return (
+    <div>
+      <NewsList news={news} />
+    </div>
+  );
+}
+
+export default Homepage;
